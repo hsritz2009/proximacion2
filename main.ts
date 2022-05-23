be3f24c8e02e4a7e4d651abd8f79cb47d2a8ac2d
hsritz2009 <98743112+hsritz2009@users.noreply.github.com>
2022-05-23 11:28:04 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+radio.onReceivedString(function (receivedString) {
+    led.plotBarGraph(
+    Math.map(0, 0, 1023, 0, 4),
+    0
+    )
+})
+radio.setGroup(222)
+basic.forever(function () {
+	
+})
